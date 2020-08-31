ff12d6658a7716d6af49fefe13256325c879a171
Pouriya Tajik <pouriya.un@gmail.com>
2020-08-31 10:48:55 +0430

    edit heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,6 +10,7 @@ const middlewares = [logger];
 // if(process.env.NODE_ENV === 'development') {
 //     middlewares.push(logger);
 // }
+//
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
